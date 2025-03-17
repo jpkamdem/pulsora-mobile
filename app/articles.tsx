e594f896dcf5572743d3e02fc31b591b163de6f0
sanjy <sanjy.boot@gmail.com>
2025-03-17 13:26:15 +0100

    debut articles

--- app/articles.tsx
+++ app/articles.tsx
@@ -5,7 +5,11 @@ export default function Articles() {
   return (
     <>
       <Navbar />
-      <Text>articles works!</Text>
+      <Text>Nutrition</Text>
+      <Text>Petits Conseils Nutrition</Text>
+      <Text>Équipement</Text>
+      <Text>Les équipements de football sont essentiels pour assurer la sécurité, la performance et le confort des joueurs tout au long du match. 
+      Chaque élément est conçu pour répondre aux besoins spécifiques du jeu et des conditions de terrain.</Text>
     </>
   );
 }
